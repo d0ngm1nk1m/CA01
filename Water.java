195ae86d330237b3968472540e04da5339efd910
d0ngm1nk1m <dmk218@naver.com>
2019-10-05 12:37:54 +0900

    base

---
+++ Water.java
@@ -0,0 +1,19 @@
+package CA01;
+
+class Water {
+	
+	int state;
+	double energy;
+	
+	Water(){
+		
+	}
+	
+	Water(int state, double energy){
+		
+		this.state = state;
+		this.energy = energy;
+		
+	}
+	
+}
